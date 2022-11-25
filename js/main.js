ca1ef8a237339e8d4dc1939261a400c7809fde58
marzepanh <marzepanh@gmail.com>
2022-11-26 01:21:14 +0300

    Module 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 function getRandomInt(first, last) {
   if (first >= last) {
@@ -12,5 +13,68 @@ function checkLength(str, len) {
   return str.len <= len;
 }
 
-getRandomInt(5, 25);
-checkLength('Hello World!', 20);
+const DESCRIPTIONS = [
+  'My first photo here',
+  'On vacation',
+  'Healthy breakfast',
+  '2 years progress',
+  'Cute night',
+  'Lori ipsum',
+  'ara fi',
+  'My favorite book',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const USED_ID = [];
+const MAX_ID = 100000;
+const POSTS_AMOUNT = 25;
+
+function getRandom() {
+  return this[getRandomInt(0, this.length - 1)];
+}
+
+function getRandomId() {
+  const id = getRandomInt(0, MAX_ID);
+  if (id in USED_ID) {
+    getRandomId();
+  }
+  USED_ID.push(id);
+  return id;
+}
+
+const createComment = () => ({
+  id: getRandomId(),
+  avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+  message: getRandom.call(MESSAGES),
+  name: getRandom.call(NAMES),
+});
+
+const createPost = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: getRandom.call(DESCRIPTIONS),
+  likes: getRandomInt(15, 200),
+  comments: Array.from({length: getRandomInt(5, 30)}, createComment),
+});
+
+const posts = Array.from({length: POSTS_AMOUNT}, (v, i) => createPost(i + 1));
+
